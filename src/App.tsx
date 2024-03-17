77b65fe13ed07aab62b77912f3df6f3c99443477
Donghwan Oh <imodh97@gmail.com>
2024-03-17 17:27:42 +0900

    #8.12

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,63 @@
 import styled from "styled-components";
-import {
-  motion,
-  useMotionValue,
-  useMotionValueEvent,
-  useScroll,
-  useTransform,
-} from "framer-motion";
-import { useRef } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { useRef, useState } from "react";
 
 const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 `;
 
 const Box = styled(motion.div)`
-  width: 200px;
+  width: 300px;
   height: 200px;
   background-color: white;
   border-radius: 40px;
+  position: absolute;
+  top: 160px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
 `;
 
-const Svg = styled.svg`
-  width: 300px;
-  height: 300px;
-  color: white;
-  path {
-    stroke: white;
-    stroke-width: 2;
-  }
-`;
-
-const svg = {
-  start: { pathLength: 0, fill: "rgba(255,255,255,0)" },
-  end: {
-    pathLength: 1,
-    fill: "rgba(255,255,255,1)",
+const boxVariants = {
+  start: {
+    opacity: 0,
+    scale: 0,
+  },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    rotateZ: 360,
+  },
+  leaving: {
+    opacity: 0,
+    scale: 0,
+    y: 20,
   },
 };
 
-const boxVariants = {};
-
 function App() {
-  return <Wrapper></Wrapper>;
+  const [showing, setShowing] = useState(false);
+  const toggleShowing = () => {
+    setShowing((prev) => !prev);
+  };
+  return (
+    <Wrapper>
+      <button onClick={toggleShowing}>Click</button>
+      <AnimatePresence>
+        {showing ? (
+          <Box
+            variants={boxVariants}
+            initial="start"
+            animate="visible"
+            exit="leaving"
+          />
+        ) : null}
+      </AnimatePresence>
+    </Wrapper>
+  );
 }
 
 export default App;
